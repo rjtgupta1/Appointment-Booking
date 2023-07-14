8d695e9d8e8884bc51fd85823a689513c31a725f
Rajat <imrjtgupta@gmail.com>
2023-07-14 11:30:37 +0530

    Intialized Backend Server

---
+++ server/index.js
@@ -0,0 +1,11 @@
+import dotenv from 'dotenv'
+import express from "express";
+
+dotenv.config({path:"./config/config.env"})
+const PORT = process.env.PORT;
+
+const app = express();
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on ${PORT}`);
+})
